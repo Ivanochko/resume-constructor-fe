798b12ef191f8534b063212cfdef9ed2daafaca5
Vasyl-Ivanochko <vasyl.ivanochko@xa.epicgames.com>
2022-11-10 15:09:58 +0200

    Fix build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,27 +7,13 @@ import {SidebarComponent} from './layout/sidebar/sidebar.component';
 import {ContentComponent} from './layout/content/content.component';
 import {HomeComponent} from './home/home.component';
 import {MatIconModule} from '@angular/material/icon';
-import {SkillsComponent} from './modules/skills/skills.component';
-import {AccountComponent} from './modules/account/account.component';
-import {ExperienceComponent} from './modules/experience/experience.component';
-import {EducationComponent} from './modules/education/education.component';
-import {SettingsComponent} from './modules/settings/settings.component';
-import {CertificationsCoursesComponent} from './modules/certifications-courses/certifications-courses.component';
-import {GenerateResumeComponent} from './modules/generate-resume/generate-resume.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     ContentComponent,
-    HomeComponent,
-    SkillsComponent,
-    AccountComponent,
-    ExperienceComponent,
-    EducationComponent,
-    SettingsComponent,
-    CertificationsCoursesComponent,
-    GenerateResumeComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
