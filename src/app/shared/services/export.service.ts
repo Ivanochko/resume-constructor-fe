aeff0deec3b4920ba0fd37c16aa6dee57d972f13
Vasyl-Ivanochko <vasyl.ivanochko@xa.epicgames.com>
2023-05-09 17:49:24 +0300

    24 - UI: Move PDF exporting to API

---
+++ src/app/shared/services/export.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {HttpService} from "./http.service";
+import {Observable} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExportService {
+  apiBaseUrl: string = "http://localhost:8080"
+
+  constructor(private httpService: HttpService,
+              private httpClient: HttpClient) {
+  }
+
+  export(): Observable<any> {
+    const headers = new HttpHeaders().set('Content-Type', 'application/pdf; charset=utf-8');
+    let options: any = {
+      headers: headers,
+      responseType: 'blob'
+    }
+    return this.httpClient.get<any>(this.apiBaseUrl + "/users/export/current", options)
+  }
+
+}
